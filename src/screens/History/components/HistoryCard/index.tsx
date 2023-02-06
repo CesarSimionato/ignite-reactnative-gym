9df138e2563da6ec6f8d89b5666a484669ecb93d
cesarmsimionato <cesar.simionato@unifeob.edu.br>
2023-02-06 15:42:31 -0300

    history

---
+++ src/screens/History/components/HistoryCard/index.tsx
@@ -0,0 +1,41 @@
+import { HStack, VStack, Heading, Text } from "native-base"
+
+export const HistoryCard: React.FC = () => {
+  return (
+    <HStack
+      bg="gray.600"
+      mb={3}
+      w="full"
+      px={5}
+      py={4}
+      rounded="md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr="5">
+        <Heading
+          color="white"
+          fontSize="md"
+          textTransform="capitalize"
+        >
+          Costas
+        </Heading>
+
+        <Text
+          color="gray.100"
+          fontSize="lg"
+          numberOfLines={1}
+        >
+          Puxada Frontal
+        </Text>
+      </VStack>
+
+      <Text
+        color="gray.300"
+        fontSize="md"
+      >
+        08:56
+      </Text>
+    </HStack>
+  )
+}
--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,9 +1,57 @@
-import { Center, Text } from "native-base"
+import { useState } from "react"
+
+import { VStack, SectionList, Heading, Text } from "native-base"
+
+import { ScreenHeader } from "@components/ScreenHeader"
+
+import { HistoryCard } from "./components/HistoryCard"
 
 export const History: React.FC = () => {
+  const [exercises, setExercises] = useState([
+    {
+      title: "04.02.23",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+    {
+      title: "05.02.23",
+      data: ["Puxada frontal"],
+    },
+  ])
+
   return (
-    <Center flex={1}>
-      <Text color="white">History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        px={8}
+        showsVerticalScrollIndicator={false}
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            mt={10}
+            mb={3}
+            color="gray.200"
+            fontSize="md"
+          >
+            {section.title}
+          </Heading>
+        )}
+        ListEmptyComponent={() => (
+          <Text
+            color="gray.100"
+            textAlign="center"
+          >
+            Não há exercícios registrados ainda
+            {"\n"}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+      />
+    </VStack>
   )
 }
